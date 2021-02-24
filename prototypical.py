578349f07898e6bd82cf8dc8259cbeca63979ad8
Ludvig <ludvigkarstens@hotmail.com>
2021-02-24 15:50:53 +0100

    Add dataset and embedding architecture

--- prototypical.py
+++ prototypical.py
@@ -1,7 +1,12 @@
 """
 """ # TODO: docstring
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
+from torchvision.transforms import Compose, Resize, RandomRotation, ToTensor
+
+torch.manual_seed(1234)
 
 samples = []
 labels = []
@@ -39,7 +44,6 @@ N_Q = 5
 #         - 1200 chars plus rotations for training (4800 classes[?] in total)
 #         - embedding architecture: 4 conv blocks of [64-filter 3x3 conv, BN, ReLU, 2x2 max-pooling]
 #         - Train using Adam with initial learning rate of 10^-3, halved every 2000 episodes.
-#     - Splits proposed by Ravi and Larochelle [22]
 # - Accuracy averaged over 1000 randomly generated episodes from test set.
 # - 5-way accuracy:
 #     - 1-shot: 98.8% 
@@ -47,3 +51,34 @@ N_Q = 5
 # - 20-way accuracy:
 #     - 1-shot: 96.0%
 #     - 5-shot: 98.9%
+
+data = torchvision.datasets.Omniglot(root='omniglot', download=True)
+data = list(data)
+X, y = zip(*data) # TODO: use this to make code cleaner
+train_data = data[:1200]
+test_data = data[1200:]
+
+augmented_train_data = []
+for deg in [0, 90, 180, 270]:
+    transform = Compose([
+        Resize((28,28)),
+        RandomRotation((deg,deg)),
+        ToTensor()
+    ])
+    augmented_data = [(transform(x[0]), x[1]) for x in train_data]
+    augmented_train_data += augmented_data
+
+# TODO: Figure out padding_mode for conv2d layer
+conv_block = [
+    nn.Conv2d(64, 64, 3, padding=1),
+    nn.BatchNorm2d(num_features=64),
+    nn.ReLU(),
+    nn.MaxPool2d(2)
+]
+embed = nn.Sequential(
+    nn.Conv2d(1, 64, 3, padding=1),
+    nn.BatchNorm2d(num_features=64),
+    nn.ReLU(),
+    nn.MaxPool2d(2),
+    *(3*conv_block) # repeat conv_block 3 times
+)
