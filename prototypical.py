a072a390f762c1b62d906c523c47b9e32d1743cc
Ludvig <ludvigkarstens@hotmail.com>
2021-02-24 18:38:19 +0100

    Begun implementation

--- prototypical.py
+++ prototypical.py
@@ -10,35 +10,70 @@ from torchvision.transforms import Compose, Resize, RandomRotation, ToTensor
 
 torch.manual_seed(1234)
 
-samples = []
-labels = []
-N = len(samples)
-classes = torch.unique(labels)
-K = len(classes)
+class PrototypicalNetwork(nn.Module):
+    def __init__(self, n_classes, embed, embed_shape, d=torch.cdist):
+        """
+        :param: n_classes () - number of classes
+        :param: embed () - embedding function 
+        :param: embed_shape () - shape of the embedded output
+        :param: d () - distance metric (default: euclidean)
+        """ # TODO: docstring
+        self.embed = embed
+        self.d = d
+        self.prototypes = torch.empty(n_classes, *embed_shape)
+    
+    def forward(self, x):
+        x = self.embed(x)
+        n_samples = len(x)
+        n_prototypes = len(self.prototypes)
+        # XXX: this reshaping trick may no longer be necessary
+        dist = self.d(x.view(n_samples, -1), self.prototypes.view(n_prototypes, -1))
+        return torch.softmax(-dist, dim=1)
 
-# distance metric
-d = lambda a, b: torch.sum(torch.square(a - b))
-# number of classes per episode
-N_C = 5
-# number of support examples per class
-N_S = 5
-# number of query examples per class
-N_Q = 5
+    def episode(samples, labels, n_way, n_shot, n_query):
+        """
+        :param: n_way - no. of classes used in episode (N_c)
+        :param: n_shot - no. of support samples used for each class (N_S)
+        :param: n_query - no. of query samples used for each class (N_Q)
+        typical recommendations: try to match params with test-time specs. E.g. for 1-shot learning with 5 classes in test set, use N_C = 5 and N_S = 1
+        it can be beneficial to use higher N_C
+        usually best to have N_S equal for training and testing
+        """ # TODO: write docstring
+        classes = torch.unique(labels)
+        n_samples = len(samples) # N
+        n_classes = len(classes) # K
 
-# typical recommendations: try to match params with test-time specs. E.g. for 1-shot learning with 5 classes in test set, use N_C = 5 and N_S = 1
-# it can be beneficial to use higher N_C
-# usually best to have N_S equal for training and testing
+        chosen_classes = classes[torch.randperm(n_classes)][:n_way]
+        for c in chosen_classes:
+            c_samples = samples[labels == c]
+            # pick random samples by shuffling and then choosing first few
+            c_samples = c_samples[torch.randperm(len(c_samples))]
+            support = c_samples[:n_shot]
+            self.prototypes[c] = torch.mean(self.embed(support), dim=0) 
+            queries = c_samples[n_shot:n_shot+n_query]
+            # TODO: aggregate prototypes and queries
+            # TODO: figure out how to handle new classes
+        
+        dist = self.d(queries, self.prototypes)
+        loss = F.cross_entropy(-dist, labels) #XXX: is this a true rewrite of the loss?
+        #for query in Q:
+        #    loss += -self(query) # XXX: should add log
 
+        return loss
 
-# episode(samples, labels, classes):
-# 1. pick N_C classes from `classes`
-# 2. for every class:
-# 3.    S_K <- select N_S points from samples with class
-# 4.    Q_K <- select N_S points from (samples\S_K) with class (XXX: shouldn't it just be the remaining instead of sampling N_Q points?)
-# 5.    c_k <- average of f_theta(x_i) # compute prototype
-# 6. for every query sample:
-# 7.    loss += -log p(y=k|x)
-
+        # 1. pick N_C classes from `classes`
+        # 2. for every class:
+        # 3.    S_K <- select N_S points from samples with class
+        # 4.    Q_K <- select N_S points from (samples\S_K) with class (XXX: shouldn't it just be the remaining instead of sampling N_Q points?)
+        # 5.    c_k <- average of f_theta(x_i) # compute prototype
+        # 6. for every query sample:
+        # 7.    loss += -log p(y=k|x)
+    
+    def fit(self, x, y):
+        pass
+    
+    def predict(self, x, y):
+        pass
 
 # Results:
 # - Dataset: Omniglot [7] (1263 chars, 20 examples of each char, from 50 alphabets)
